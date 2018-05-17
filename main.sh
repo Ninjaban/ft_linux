f3817ecd885ced8d104e96131d39b9c144f9ff8f
Jonathan Carra <jcarra@student.42.fr>
2018-05-17 14:40:55 +0200

    update

--- main.sh
+++ main.sh
@@ -58,32 +58,3 @@ echo "install tar..." && install_packet tar &> ${DIRLOG}tar.log
 echo "install texinfo..." && install_packet texinfo &> ${DIRLOG}texinfo.log
 echo "install util-linux..." && install_util-linux &> ${DIRLOG}util_linux.log
 echo "install xz-utils..." && install_packet xz &> ${DIRLOG}xz.log
-
-
-su root
-mkdir -pv $LFS/{dev,proc,sys,run}
-
-mknod -m 600 $LFS/dev/console c 5 1
-mknod -m 666 $LFS/dev/null c 1 3
-
-mount -v --bind /dev $LFS/dev
-
-mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
-mount -vt proc proc $LFS/proc
-mount -vt sysfs sysfs $LFS/sys
-mount -vt tmpfs tmpfs $LFS/run
-
-if [ -h $LFS/dev/shm ]; then
-	mkdir -pv $LFS/$(readlink $LFS/dev/shm)
-fi
-
-
-chroot "$LFS" /tools/bin/env -i \
-	HOME=/root                  \
-	TERM="$TERM"                \
-	PS1='(lfs chroot) \u:\w\$ ' \
-	PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
-	/tools/bin/bash --login +h
-
-
-
